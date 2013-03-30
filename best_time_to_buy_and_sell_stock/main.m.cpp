fbc3669d5b4e118b99e645ca46925fdcd5598595
alsofly <alsofly@gmail.com>
2013-03-30 02:15:36 -0400

    solved [best time to buy and sell stock]

---
+++ best_time_to_buy_and_sell_stock/main.m.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+   int maxProfit(vector<int>& prices)
+   {
+      if ( prices.size() <= 1 ) return 0;
+
+      int maxValue = prices[1] - prices[0];
+      int maxOnRight = maxValue;
+      int i = 0;
+      for ( ; i < prices.size() - 1; ++i )
+      {
+         int priceDiff = prices[i+1] - prices[i];
+         maxValue = max(maxValue, priceDiff);
+         maxOnRight = priceDiff;
+         if ( priceDiff > 0 ) break;
+      }
+
+      int tempDiffSum = 0;
+      for ( ++i; i < prices.size() - 1; ++i )
+      {
+         int priceDiff = prices[i+1] - prices[i];
+         if ( priceDiff <= 0 ) tempDiffSum += priceDiff;
+         else
+         {
+            maxOnRight = priceDiff + max(0, maxOnRight + tempDiffSum);
+            maxValue = max(maxValue, maxOnRight);
+            tempDiffSum = 0;
+         }
+      }
+
+      return maxValue > 0 ? maxValue : 0;
+   }
+};
