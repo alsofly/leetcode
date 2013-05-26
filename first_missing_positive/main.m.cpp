0e3e46736d6e1fdbcf1e0f87d571b35541896c64
alsofly <alsofly@gmail.com>
2013-05-25 23:01:14 -0400

    solved [first missing positive]

---
+++ first_missing_positive/main.m.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+   int firstMissingPositive(int A[], int n) {
+      // Start typing your C/C++ solution below
+      // DO NOT write int main() function
+
+      // Move non-positive number to the end
+      int tail(n - 1);
+      for ( int i = 0; i < n; ++i )
+      {
+         
+         if ( i >= tail ) break;
+         while ( A[i] <= 0 && i < tail ) 
+         {
+            A[i] = A[i]^A[tail];
+            A[tail] = A[i]^A[tail];
+            A[i] = A[i]^A[tail];
+            --tail;
+         }
+      }
+      if ( A[tail] <= 0 ) --tail;
+
+      // Go through the whole array, 
+      // mark m-th element negative if meet m ( when m < n )
+      for ( int i = 0; i <= tail; ++i )
+      {
+         if ( abs(A[i]) <= tail + 1
+           && A[abs(A[i])-1] > 0 )
+         {
+            A[abs(A[i])-1] *= -1;
+         }
+      }
+
+      // Go through the whole array again,
+      // the first positve nubmer's index is the first
+      // missing positive
+      for ( int i = 0; i <= tail; ++i )
+      {
+         if ( A[i] > 0 ) return i+1;
+      }
+
+      return tail+2;
+   }
+};
+
+int main(int argc, const char *argv[])
+{
+   Solution sol;
+   int A[] = {3,2,1,6,7,4};
+   cout << sol.firstMissingPositive(A, sizeof(A)/sizeof(A[0])) << endl;
+   return 0;
+}
+
+
