04077dab85bdf2c0161a8771e33b2ceb3ceee868
alsofly <alsofly@gmail.com>
2013-05-28 02:02:11 -0400

    solved [spiral matrix 2]

---
+++ spiral_matrix_2/main.m.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+   vector<vector<int> > generateMatrix(int n) {
+      // Start typing your C/C++ solution below
+      // DO NOT write int main() function
+      vector<vector<int> > rtn;
+      rtn.resize(n, vector<int>(n, 1));
+
+      int len(n), origin(0), firstNum(1);
+      while ( len > 0 )
+      {
+         firstNum = paint_peri(rtn, origin, len, firstNum);
+         ++origin;
+         len -= 2;
+      }
+
+      return rtn;
+   }
+
+   // return the number next to the last number
+   int paint_peri(
+         vector<vector<int> >& matrix,
+         int origin,
+         int len,
+         int firstNum)
+   {
+      if ( len == 1 ) 
+      {
+         matrix[origin][origin] = firstNum;
+         return firstNum + 1;
+      }
+
+      for ( int i = 0; i < len - 1; ++i )
+      {
+         matrix[origin][origin+i] = firstNum + i;
+         matrix[origin+i][origin+len-1] = firstNum + len - 1 +i;
+         matrix[origin+len-1][origin+len-1-i] = firstNum + 2*(len-1) +i;
+         matrix[origin+len-1-i][origin] = firstNum + 3*(len-1) +i;
+      }
+
+      return firstNum + 4*(len - 1);
+   }
+
+};
+
+int main(int argc, const char *argv[])
+{
+   Solution sol;
+   
+   return 0;
+}
--- unique_paths_2/main.m.cpp
+++ unique_paths_2/main.m.cpp
@@ -5,44 +5,44 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        int len = obstacleGrid.size();
-        int width = obstacleGrid[0].size();
-        for ( int i = 0; i < len; ++i )
-        {
-            for ( int j = 0; j < width; ++j )
+   int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+      // Start typing your C/C++ solution below
+      // DO NOT write int main() function
+
+      int len = obstacleGrid.size();
+      int width = obstacleGrid[0].size();
+      for ( int i = 0; i < len; ++i )
+      {
+         for ( int j = 0; j < width; ++j )
+         {
+            int& curr = obstacleGrid[i][j];
+            if ( curr == 1 )
             {
-                int& curr = obstacleGrid[i][j];
-                if ( curr == 1 )
-                {
-                    curr = 0;
-                }
-                else if ( i == 0 && j == 0 ) 
-                {
-                    curr = 1;
-                }
-                else if ( i == 0 )
-                {
-
-                        curr = obstacleGrid[0][j-1];
-                }
-                else if ( j == 0 )
-                {
-
-                        curr = obstacleGrid[i-1][0];
-                }
-                else
-                {
-                    curr = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
-                }
+               curr = 0;
             }
-        }
-        
-        return obstacleGrid[len-1][width-1];
-    }
+            else if ( i == 0 && j == 0 ) 
+            {
+               curr = 1;
+            }
+            else if ( i == 0 )
+            {
+
+               curr = obstacleGrid[0][j-1];
+            }
+            else if ( j == 0 )
+            {
+
+               curr = obstacleGrid[i-1][0];
+            }
+            else
+            {
+               curr = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+            }
+         }
+      }
+
+      return obstacleGrid[len-1][width-1];
+   }
 };
 
 int main(int argc, const char *argv[])
